f9ff92d2189cd298df0b5db642029786b6fd9a48
Sebastian Ruiz <sruiz@carfaxmx.com.mx>
2024-09-24 15:42:57 -0600

    [ADD] prueba de concepto added

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,20 @@ import { AppModule } from './app.module';
 import { promises as fs } from 'fs';
 
 async function bootstrap() {
-  const [ca, key, cert] = await Promise.all([
-    fs.readFile('certs/ca.crt'), 
-    fs.readFile('certs/server.key'),
-    fs.readFile('certs/server.crt')
-  ]);
+  // const [ca, key, cert] = await Promise.all([
+  //   fs.readFile('certs/ca.crt'), 
+  //   fs.readFile('certs/server.key'),
+  //   fs.readFile('certs/server.crt')
+  // ]);
 
   const app = await NestFactory.create(AppModule,{
-    httpsOptions: {
-      ca: ca.toString(),
-      key: key.toString(),
-      cert: cert.toString(),
-      requestCert: true,
-    }
+    // httpsOptions: {
+    //   ca: ca.toString(),
+    //   key: key.toString(),
+    //   cert: cert.toString(),
+    //   requestCert: true,
+    // }
   });
-  await app.listen(3000);
+  await app.listen(3005);
 }
 bootstrap();
